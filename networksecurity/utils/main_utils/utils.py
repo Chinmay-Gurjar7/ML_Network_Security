2d51fcb059cff812902255991810e813c4032f7f
Chinmay Gurjar <gurjarchinmay01@gmail.com>
2026-07-29 18:19:10 +0530

    Data Transformation Initiated

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -24,3 +24,13 @@ def write_yaml_file(file_path:str, content = object, replace = bool)->None:
                 yaml.dump(content, yaml_file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path:str, array:np.array)->None:
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
