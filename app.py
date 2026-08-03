fdec579ba8775360f0035e1aabb1db36c6c5ba25
Chinmay Gurjar <gurjarchinmay01@gmail.com>
2026-08-03 19:32:16 +0530

    Pipeline Completed

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-import os, sys
+import sys
+import os
 
 import certifi
 ca = certifi.where()
 
 from dotenv import load_dotenv
 load_dotenv()
-mongo_db_url = os.getenv("MONGO_DB_KEY")
-print(f"MongoDB URL: {mongo_db_url}")
+mongo_db_url = os.getenv("MONGODB_URL_KEY")
+print(mongo_db_url)
 import pymongo
-from networksecurity.pipeline.training_pipeline import TrainingPipeline
 from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.logging import logger
+from networksecurity.logging.logger import logging
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
-from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile,Request
 from uvicorn import run as app_run
-from fastapi.responses import Response, JSONResponse
+from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
-import numpy as np
 
 from networksecurity.utils.main_utils.utils import load_object
+
+from networksecurity.utils.main_utils.ml_utils.model.estimator import NetworkModel
+
 
 client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 
@@ -30,7 +33,7 @@ from networksecurity.constant.training_pipeline import DATA_INGESTION_DATABASE_N
 database = client[DATA_INGESTION_DATABASE_NAME]
 collection = database[DATA_INGESTION_COLLECTION_NAME]
 
-app = FastAPI(title="Network Security API", version="1.0.0")
+app = FastAPI()
 origins = ["*"]
 
 app.add_middleware(
@@ -41,6 +44,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
+
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -48,11 +54,35 @@ async def index():
 @app.get("/train")
 async def train_route():
     try:
-        train_pipeline = TrainingPipeline()
+        train_pipeline=TrainingPipeline()
         train_pipeline.run_pipeline()
-        return JSONResponse(content={"message": "Training pipeline started successfully."}, status_code=200)
+        return Response("Training is successful")
     except Exception as e:
-        raise NetworkSecurityException(e, sys) from e
+        raise NetworkSecurityException(e,sys)
     
-if __name__ == "__main__":
-    app_run(app, host="localhost", port=8000, reload=True)
+@app.post("/predict")
+async def predict_route(request: Request,file: UploadFile = File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    
+if __name__=="__main__":
+    app_run(app,host="0.0.0.0",port=8000)
